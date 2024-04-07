8e260506fd929d3ca1018a158d1429f8b92e27fc
Anton Src <anton.source@gmail.com>
2024-04-07 23:11:16 +0700

    add possibility of working with several lifts

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 import { computed, ref, onMounted, reactive } from 'vue'
 
 const projSettings = {
-    liftCount: 1,
+    shaftCount: 2,
     floorCount: 7,
 };
 
@@ -11,60 +11,70 @@ for (let i = projSettings.floorCount; i > 0; i--) {
     floors.push(i)
 }
 
-let inProc = ref(false);
+let shaftsArr = [];
+let objLift = {};
+for (let i = 0; i < projSettings.shaftCount; i++) {
+    shaftsArr.push(i);
+    objLift[i] = {
+        floor: 1,
+        realFloor: projSettings.floorCount,
+        atWork: false,
+        nextFloor: 0,
+    };
+}
+let queueOfLifts = reactive(objLift);
 
-let liftOnFloor = ref(1);
-let curFloor = ref(projSettings.floorCount + 1 - liftOnFloor.value);
+let liftRef = ref([]);
+let shafts = ref(shaftsArr);
+
 let queueOfCalls = reactive(new Set());
-
-let liftRef = ref(null);
 
 let direction = ref(null);
 
-async function stopWaiting(queue, q) {
-    direction.value = 0;
-    console.log('приехали на ' + q.floor);
-    console.log(q.event.target)
-    queue.delete(q);
-    q.event.target.style.backgroundColor = 'bisque';
 
-    liftRef.value.className = 'liftBlink';
-
-    await delay(3000);
-    liftRef.value.className = 'lift';
+function getNearestFreeLift(button) {
+    let diff = projSettings.floorCount;
+    let nearest = -1;
+    for (const lift in queueOfLifts) {
+        if (queueOfLifts[lift]['atWork']) continue;
+        else if (diff > Math.abs(queueOfLifts[lift].floor - button)) {
+            nearest = queueOfLifts[lift].floor;
+            diff = Math.abs(queueOfLifts[lift].floor - button);
+        }
+    }
+    return nearest;
 }
 
-let nextFloor = ref(0);
 
-async function moveLift(queue, liftPos) {
-    inProc.value = true;
-    // console.log('A')
+async function stopWaiting(queue, q) {
+    direction.value = 0;
+    queue.delete(q);
+    q.event.target.style.backgroundColor = 'bisque';
+    liftRef.value[1].className = 'liftBlink';
+    await delay(3000);
+    liftRef.value[1].className = 'lift';
+}
+
+async function moveLift(queue, lift) {
+    queueOfLifts[lift].atWork = true;
     for (let q of queue) {
-        // console.log('B')
-        direction.value = getDirection(liftPos, q.floor);
-        let counts = Math.abs(liftPos - q.floor);
-        
-        nextFloor.value = q.floor;
+        direction.value = getDirection(queueOfLifts[lift].floor, q.floor);
+        let counts = Math.abs(queueOfLifts[lift].floor - q.floor);
+        queueOfLifts[lift].nextFloor = q.floor;
 
         for (let i = counts; i > 0; i--) {
             await delay(1000);
-            liftPos = liftPos + direction.value;
-            curFloor.value = projSettings.floorCount + 1 - liftPos;
-            liftOnFloor.value = liftPos;
-            // console.log(liftPos);
-            // console.log(sec + ' мс');
+            queueOfLifts[lift].floor = queueOfLifts[lift].floor + direction.value;
+            queueOfLifts[lift].realFloor = projSettings.floorCount +1 - queueOfLifts[lift].floor;
         }
         await stopWaiting(queue, q);
-        console.log(queue.size)
     }
-    inProc.value = false;
+    queueOfLifts[lift].atWork = false;
 }
 
 
 
 function callLift(e, floorBtn) {
-    console.log('--------',e.target)
-
     let objCall = {
         floor: floorBtn,
         event: e,
@@ -72,11 +82,11 @@ function callLift(e, floorBtn) {
     if (!queueOfCalls.has(objCall)) {
         e.target.style.backgroundColor = 'red';
         queueOfCalls.add(objCall);
-        if (inProc.value == false) {
-            moveLift(queueOfCalls, liftOnFloor.value);
+        let nearestLift = getNearestFreeLift(floorBtn);
+        if (queueOfLifts[nearestLift].atWork == false) {
+            moveLift(queueOfCalls, nearestLift);
         }
     }
-    // console.log(queueOfCalls);
 }
 
 function delay(ms) {
@@ -87,10 +97,8 @@ function getDirection(from, to) {
     let dir = null;
     if (from < to) {
         dir = 1;
-        // console.log('направление вверх ↑');
     } else {
         dir = -1;
-        // console.log('направление вниз ↓');
     }
     return dir;
 }
@@ -104,17 +112,15 @@ function getDirection(from, to) {
                 <span :id="floor + '_btn'" className="btn" @click="callLift($event, floor)">Вызвать</span>
             </div>
         </div>
-
-        <div className="liftShaft">
-            <div className="liftExits" :id="floor + '_floor'" v-for="floor in floors">
+        
+        <div className="liftShaft" v-for="shaft in shafts">
+            <div className="liftExits" v-for="floor in floors">
                 <span :style="{ fontSize: 2 + 'rem' }">{{ floor }}</span>
             </div>
-
-            <div ref="liftRef" className="lift">
-                <span v-if="direction == 1">Вверх ↑ на {{ nextFloor }} этаж</span>
-                <span v-else-if="direction == -1">Вниз ↓ на {{ nextFloor }} этаж</span>
+            <div ref="liftRef" className="lift" :style="{gridRowStart: queueOfLifts[shaft].realFloor, gridRowEnd: queueOfLifts[shaft].realFloor}">
+                <span v-if="direction == 1">Вверх ↑ на {{ queueOfLifts[shaft].nextFloor }} этаж</span>
+                <span v-else-if="direction == -1">Вниз ↓ на {{ queueOfLifts[shaft].nextFloor }} этаж</span>
                 <span v-else></span>
-
             </div>
         </div>
     </div>
@@ -132,18 +138,12 @@ function getDirection(from, to) {
     height: 40px;
 }
 
-
-
 .callButtons {
     display: flex;
     flex-direction: column-reverse;
     width: 10%;
     border: solid 4px rgb(23, 251, 244);
 }
-
-
-
-
 
 .liftShaft {
     display: grid;
@@ -160,7 +160,7 @@ function getDirection(from, to) {
 }
 
 .lift {
-    grid-row: v-bind('curFloor')/v-bind('curFloor');
+    /* grid-row: v-bind('curFloor')/v-bind('curFloor'); */
     background-color: rgba(49, 235, 127, 0.508);
     width: 100%;
     height: 100%;
@@ -172,12 +172,12 @@ function getDirection(from, to) {
 }
 
 .liftBlink {
-    grid-row: v-bind('curFloor')/v-bind('curFloor');
+    /* grid-row: v-bind('curFloor')/v-bind('curFloor'); */
     width: 100%;
     height: 100%;
     position: absolute;
 
-    animation-name: headerBlink;
+    animation-name: blink;
     animation-duration: 1.5s;
     animation-timing-function: linear;
     animation-iteration-count: infinite;
@@ -187,21 +187,13 @@ function getDirection(from, to) {
     align-items: center;
 }
 
-
 .btn {
     cursor: pointer;
     background-color: bisque;
     margin: 5px;
 }
 
-
-
-
-
-
-
-
-@keyframes headerBlink {
+@keyframes blink {
     from {
         background: rgba(255, 255, 255, 0);
         color: rgba(0, 0, 0, 0);
@@ -215,11 +207,4 @@ function getDirection(from, to) {
         color: rgba(0, 0, 0, 0);
     }
 }
-
-
-
-
-
-
-
 </style>
